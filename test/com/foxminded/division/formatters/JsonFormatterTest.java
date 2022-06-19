ccd9299bf16c9b45760bf3b0ab2b0586f9f88271
Valhuk <vladrockyvlad@gmail.com>
2022-06-19 22:17:46 +0300

    tests fixed.

--- test/com/foxminded/division/formatters/JsonFormatterTest.java
+++ test/com/foxminded/division/formatters/JsonFormatterTest.java
@@ -2,62 +2,29 @@ package com.foxminded.division.formatters;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
-import com.foxminded.division.processors.DivisionProcessor;
+import com.foxminded.division.processors.DivisionStep;
 
 class JsonFormatterTest {
-	DivisionProcessor processor = new DivisionProcessor();
+	private List<DivisionStep> steps = Arrays.asList(new DivisionStep(7, 4), new DivisionStep(38, 36),
+			new DivisionStep(29, 28), new DivisionStep(14, 12), new DivisionStep(25, 24), new DivisionStep(1, null));
 	Formatter jsonFormatter = new JsonFormatter();
 
 	@Test
-	void testArithmeticExceptin_ShouldThrowArithmeticException_WhenDivisorIsZero() {
-		assertThrows(ArithmeticException.class,() -> jsonFormatter.format(7, 0, processor.divide(7, 0)));
-	}
-	
-	@Test
 	void testDividing_ShouldResultOfDividing_WhenDividentIsBiggerThanDivisor() {
-		String expected = "{\r\n"
-				+ "  \"dividend\" : 78945,\r\n"
-				+ "  \"divider\" : 4,\r\n"
-				+ "  \"result\" : 19736,\r\n"
-				+ "  \"iterations\" : [ {\r\n"
-				+ "    \"dividend\" : 7,\r\n"
-				+ "    \"subtrahend\" : 4\r\n"
-				+ "  }, {\r\n"
-				+ "    \"dividend\" : 38,\r\n"
-				+ "    \"subtrahend\" : 36\r\n"
-				+ "  }, {\r\n"
-				+ "    \"dividend\" : 29,\r\n"
-				+ "    \"subtrahend\" : 28\r\n"
-				+ "  }, {\r\n"
-				+ "    \"dividend\" : 14,\r\n"
-				+ "    \"subtrahend\" : 12\r\n"
-				+ "  }, {\r\n"
-				+ "    \"dividend\" : 25,\r\n"
-				+ "    \"subtrahend\" : 24\r\n"
-				+ "  }, {\r\n"
-				+ "    \"dividend\" : 1,\r\n"
-				+ "    \"subtrahend\" : null\r\n"
-				+ "  } ]\r\n"
-				+ "}";
-		assertEquals(expected, jsonFormatter.format(78945, 4, processor.divide(78945, 4)));
-	}
-	
-	@Test
-	void testDividingWithSmallNumbers_ShouldReturnDividing_WhenDividendAndDivisorAreSmallNumbers() {
-		assertEquals("{\r\n"
-				+ "  \"dividend\" : 12,\r\n"
-				+ "  \"divider\" : 4,\r\n"
-				+ "  \"result\" : 3,\r\n"
-				+ "  \"iterations\" : [ {\r\n"
-				+ "    \"dividend\" : 12,\r\n"
-				+ "    \"subtrahend\" : 12\r\n"
-				+ "  }, {\r\n"
-				+ "    \"dividend\" : 0,\r\n"
-				+ "    \"subtrahend\" : null\r\n"
-				+ "  } ]\r\n"
-				+ "}", jsonFormatter.format(12, 4, processor.divide(12, 4)));
+		String expected = "{\r\n" + "  \"dividend\" : 78945,\r\n" + "  \"divider\" : 4,\r\n"
+				+ "  \"result\" : 19736,\r\n" + "  \"iterations\" : [ {\r\n" + "    \"dividend\" : 7,\r\n"
+				+ "    \"subtrahend\" : 4\r\n" + "  }, {\r\n" + "    \"dividend\" : 38,\r\n"
+				+ "    \"subtrahend\" : 36\r\n" + "  }, {\r\n" + "    \"dividend\" : 29,\r\n"
+				+ "    \"subtrahend\" : 28\r\n" + "  }, {\r\n" + "    \"dividend\" : 14,\r\n"
+				+ "    \"subtrahend\" : 12\r\n" + "  }, {\r\n" + "    \"dividend\" : 25,\r\n"
+				+ "    \"subtrahend\" : 24\r\n" + "  }, {\r\n" + "    \"dividend\" : 1,\r\n"
+				+ "    \"subtrahend\" : null\r\n" + "  } ]\r\n" + "}";
+		assertEquals(expected, jsonFormatter.format(78945, 4, steps));
 	}
 
 }
--- test/com/foxminded/division/formatters/SimpleFormatterTest.java
+++ test/com/foxminded/division/formatters/SimpleFormatterTest.java
@@ -2,21 +2,19 @@ package com.foxminded.division.formatters;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
-import com.foxminded.division.processors.DivisionProcessor;
+import com.foxminded.division.processors.DivisionStep;
 
 class SimpleFormatterTest {
-	DivisionProcessor processor = new DivisionProcessor();
-    Formatter simpleFormatter = new SimpleFormatter();
+	private List<DivisionStep> steps = Arrays.asList(new DivisionStep(8, 10));
+	Formatter simpleFormatter = new SimpleFormatter();
 
 	@Test
-	void testArithmeticExceptin_ShouldThrowArithmeticException_WhenDivisorIsZero() {
-		assertThrows(ArithmeticException.class,() -> simpleFormatter.format(7, 0, processor.divide(7, 0)));
-	}
-	
-	@Test
 	void testDividingWhenDivisorIsBiggerThanDividend_ShouldReturnSimpleDividing_WhenDivisorIsBiggerThanDividend() {
-		assertEquals("8/10=0.8", simpleFormatter.format(8, 10, processor.divide(8, 10)));
+		assertEquals("8/10=0.8", simpleFormatter.format(8, 10, steps));
 	}
 }
